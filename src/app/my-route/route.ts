70990e7fc68304648ecffa43318c7102c69910d2
fastsourceindia <fastsourceindia@gmail.com>
2026-01-31 17:35:51 +0530

    fix: force dynamic on custom route

--- src/app/my-route/route.ts
+++ src/app/my-route/route.ts
@@ -1,6 +1,9 @@
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 
+// ✅ This tells Next.js to skip pre-rendering this route during the build
+export const dynamic = 'force-dynamic' 
+
 export const GET = async (request: Request) => {
   const payload = await getPayload({
     config: configPromise,
