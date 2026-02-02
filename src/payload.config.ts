1a57c2b03f24316c3e36a18032fa9f587e9b50a4
fastsourceindia <fastsourceindia@gmail.com>
2026-02-02 22:20:18 +0530

    Cloudinary Prefix: Added prefix to cloudinary

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -70,6 +70,7 @@ export default buildConfig({
           collections: {
             media: {
               adapter: cloudinaryAdapter(),
+              prefix: 'payload-subjects',
             },
           },
         })
